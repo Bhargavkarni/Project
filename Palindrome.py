d87b6c536e8edf6e7f249613dd9770bdff2ee1fd
BHARGAV <bhargavkarni.marolix@gmail.com>
2023-09-19 23:27:26 +0530

    Palindrome

---
+++ Palindrome.py
@@ -0,0 +1,10 @@
+string = "madam"
+
+reverse_string = string[::-1]
+
+condition = string == reverse_string
+
+if condition:
+    print("String is Palindrome")
+else:
+    print("String is not Palindrome")
