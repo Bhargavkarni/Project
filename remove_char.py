51a091690675551ee1980afa490ba41d4ef6e491
BHARGAV <bhargavkarni.marolix@gmail.com>
2023-09-19 23:39:25 +0530

    vowel or consonant

---
+++ remove_char.py
@@ -0,0 +1,11 @@
+name = "Bhargav"
+single_char = "a"
+new_string = ""
+
+for each_char in name:
+    if each_char == 'a':
+        new_string = new_string + ""
+    else:
+        new_string = new_string + each_char
+
+print(new_string)
---
+++ vowel or consonant.py
@@ -0,0 +1,6 @@
+character = input("Enter your character :  ")
+
+if character == 'a' or character == 'e' or character == 'i' or character == 'o' or character == 'u':
+    print("Vowels")
+else:
+    print("Consonants")
